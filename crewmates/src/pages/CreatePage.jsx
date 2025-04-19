4be377afa3bbd6f1cf5bd6ed3e2f0ad8fd47f28b
Muhais Olatundun <monnkcode@gmail.com>
2025-04-19 00:39:19 -0500

    project finished

--- crewmates/src/pages/CreatePage.jsx
+++ crewmates/src/pages/CreatePage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../client';
 import { useNavigate } from 'react-router-dom';
 
 export default function CreatePage() {
--- crewmates/src/supabaseClient.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://ctanhyzwlviilnpzcpcv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0YW5oeXp3bHZpaWxucHpjcGN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMTI0MjAsImV4cCI6MjA2MDU4ODQyMH0.JZadH6VD1i5ZlsO7k_bAhyq6Ww5O2mMvIdcMYRNYU88';
-export const supabase = createClient(supabaseUrl, supabaseKey);
